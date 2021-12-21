5cc420aadd197b65f5d871b02869259a4d493547
Adriano <adriano.moreira@totvs.com.br>
2021-12-21 11:28:11 -0300

    testes

--- Entity/Unidade.cs
+++ Entity/Unidade.cs
@@ -27,6 +27,7 @@ namespace OrcaFascio.Entity
         public override string ToString()
         {
             return CodUnd + " - " + Descricao;
+            return CodUnd + " - " + Descricao2;
         }
     }
 }
